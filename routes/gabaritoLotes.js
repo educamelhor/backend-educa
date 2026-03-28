db7d609f68a5cbd57822041116d79bf04530fca1
André Santos <educa.melhor.2025@gmail.com>
2026-03-27 21:20:08 -0300

    fix: add node-fetch dep - native fetch incompatible with npm form-data streams (504 timeout)

--- routes/gabaritoLotes.js
+++ routes/gabaritoLotes.js
@@ -14,6 +14,7 @@ import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import FormData from "form-data";
+import fetch from "node-fetch";
 import pool from "../db.js";
 import { uploadFileBufferToSpaces, downloadBufferFromSpaces } from "../storage/spacesUpload.js";
 
